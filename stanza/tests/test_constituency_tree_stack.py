080ee65fffd55319c52eb1e7aeb674dffcc3f468
John Bauer <horatio@gmail.com>
2021-06-18 12:37:14 -0700

    Initial attempt at building a State and some transitions

---
+++ stanza/tests/test_constituency_tree_stack.py
@@ -0,0 +1,48 @@
+import pytest
+
+from stanza.models.constituency.tree_stack import TreeStack
+
+from stanza.tests import *
+
+pytestmark = [pytest.mark.pipeline, pytest.mark.travis]
+
+def test_simple():
+    stack = TreeStack(value=5)
+    stack = stack.push(3)
+    stack = stack.push(1)
+
+    expected_values = [1, 3, 5]
+    for value in expected_values:
+        assert stack.value == value
+        stack = stack.pop()
+    assert stack is None
+
+def test_iter():
+    stack = TreeStack(value=5)
+    stack = stack.push(3)
+    stack = stack.push(1)
+
+    stack_list = list(stack)
+    assert list(stack) == [1, 3, 5]
+
+def test_str():
+    stack = TreeStack(value=5)
+    stack = stack.push(3)
+    stack = stack.push(1)
+
+    assert str(stack) == "TreeStack(1, 3, 5)"
+
+def test_str_variant():
+    stack = TreeStack(value=5, value_to_str=lambda x: str(x+1))
+    stack = stack.push(3)
+    stack = stack.push(1)
+
+    assert str(stack) == "TreeStack(2, 4, 6)"
+
+def test_len():
+    stack = TreeStack(value=5)
+    stack = stack.push(3)
+    stack = stack.push(1)
+
+    assert len(stack) == 3
+
